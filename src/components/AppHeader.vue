1a0f9ce8ef21902652343085484a9dd08f6bca89
Taíssa <taissa.leal@estudante.ufjf.br>
2024-04-10 11:57:14 -0300

    adiciona links para páginas

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,69 +1,86 @@
 <template>
-    <header class="container">
+<header class="container">
+    <nav class="navbar navbar-expand">
+    <a href="#" class="link navbar-brand mr-auto" id="header-logo">
+        <img src="./../assets/94-946164_graduation-week-graduation-cap-icon-white.png" alt="Graduation Cap Logo" class="img d-inline-block align-top" id="logo"> SiPlanWeb
+    </a>
 
-        <nav class="navbar navbar-expand">
+    <disciplinas/>
+    <turmasCursos/>
 
-            <a href="#" class="link navbar-brand mr-auto">
-                <img src="./../assets/94-946164_graduation-week-graduation-cap-icon-white.png" alt="Graduation Cap Logo" class="img d-inline-block align-top" id="logo"> SiPlanWeb
-            </a>
+    <div class="page-selection">
+        <div>
+            <h3 @click="goToPage('/turmascursos')">Turmas-Cursos</h3>
+            <h3 @click="goToPage('/disciplinas')">Disciplinas</h3>
+        </div>
+    </div>
 
-            <div>
-                <b-button v-b-toggle.sidebar-1>Toggle Sidebar</b-button>
-                <b-sidebar id="sidebar-1" title="Sidebar" shadow>
-                <div class="px-3 py-2">
-                    <p>
-                        Teste
-                    </p>
-                </div>
-                </b-sidebar>
-            </div>
+    <div class="links mr-2">
+        <div class="selection">
+        <h3 class="selection-title">Plano atual</h3>
+        <select name="plano" id="select-plano" class="form-control" @change="$emit('select-plano', $event.target.value)">
+            <option style="display: none;">Plano atual</option>
+            <option value="" selected>Selecionar</option>
+            <option value="plano2024">2024 - 2024</option>
+            <option value="plano2023">2023 - 2023</option>
+            <option value="plano2022">2022 - 2022</option>
+            <option value="plano2021">2021 - 2021</option>
+        </select>
+        </div>
 
-            <div class="links mr-2">
-                
-                <a href="#" class="btn mr-5">Plano atual</a>
-                <select name="plano" id="select-plano" class="form-control" @change="$emit('select-plano', $event.target.value)">
-                        <option value="plano2024" selected>2024 - 2024</option>
-                        <option value="plano2023">2023 - 2023</option>
-                        <option value="plano2022">2022 - 2022</option>
-                        <option value="plano2021">2021 - 2021</option>
-                </select>
-                
-                <a href="#" class="btn mr-5">
-                    <img src="./../assets/sync.png" class="icon">
-                    Sincronizar Drive
-                </a>
-                <a href="#" class="btn mr-5">
-                    <img src="./../assets/user.png" class="icon">
-                    Usuário
-                </a>
-                <a href="#" class="btn mr-5">
-                    <img src="./../assets/download.png" class="icon">
-                    Download
-                </a>
-                <a href="#" class="btn mr-5">
-                    <img src="./../assets/logout.png" class="icon">
-                    Logout
-                </a>
-
-            </div>
-
-        </nav>
-
-    </header>
+        <div class="buttons">
+        <a href="#" class="btn mr-5">
+            <img src="./../assets/sync.png" class="icon">
+            Sincronizar Drive
+        </a>
+        <a href="#" class="btn mr-5">
+            <img src="./../assets/user.png" class="icon">
+            Usuário
+        </a>
+        <a href="#" class="btn mr-5">
+            <img src="./../assets/download.png" class="icon">
+            Download
+        </a>
+        <a href="#" class="btn mr-5">
+            <img src="./../assets/logout.png" class="icon">
+            Logout
+        </a>
+        </div>
+    </div>
+    </nav>
+</header>
 </template>
 
 <script>
-export default {
+import disciplinas from './../components/AppTable.vue'
+import turmasCursos from './../components/AppTurmasCursos.vue'
 
+export default {
+    methods: {
+        goToPage(page) {
+            this.$router.push(page);
+        }
+    },
+    components: {
+        disciplinas,
+        turmasCursos
+    }
 }
 </script>
 
 <style scoped>
 header {
+    padding: 0;
+    height: 60px;
+    min-width: 100%;
+    font-size: 100%;
+    overflow: visible; 
+    align-content: center;
+    margin: 0px 0px 0px 0px;
     background-color: #af1e23;
-    height: 100px;
-    overflow: hidden;
     border: solid transparent;
+    position: relative; 
+    z-index: 1; 
 }
 
 .container {
@@ -72,12 +89,16 @@ header {
 
 #logo {
     width: 25px;
-    height: 25px;
+    height: 22px;
+    margin-left: 50px;
+    padding: 2px;
 }
 
 .link {
     color: #fff;
-    font-size: 20px;
+    font-size: 17px;
+    display: inline-flex;
+    align-items: center;
 }
 
 .links {
@@ -86,7 +107,8 @@ header {
 
 .links a {
     color: #fff;
-    font-size: 15px;
+    font-size: 10px;
+    /* padding: 5px; */
 }
 
 .links a:focus {
@@ -95,11 +117,45 @@ header {
 }
 
 .icon {
-    width: 15px;
-    height: 15px;
+    width: 10px;
+    height: 10px;
 }
 
 .sidebar-1 {
     color: #fff;
+    width: 100px;
+    padding: 20px;
+    margin-left: 50px;
+}
+.selection {
+    display: inline-flex;
+}
+
+.plano {
+    background-color: #6c1316;
+    width: 100%;
+    height: 15px;
+    margin: 0;
+    padding: 0;
+    font-size: 5px;
+}
+
+.selection-title {
+    color: #fff;
+    font-size: 10px;
+    width: 100%;
+    margin-top: 8px;
+}
+
+#select-plano {
+    width: 100%;
+}
+
+.buttons {
+    margin-left: 50px;
+}
+
+.links-pages {
+color: #6c1316;
 }
 </style>
