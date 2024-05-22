653d77f7625d645380bc366b9dee37960d5e7f49
Taíssa <taissa.leal@estudante.ufjf.br>
2024-05-22 16:11:04 -0300

    Refatora rotas

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,18 +1,21 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 
-import Disciplinas from './../components/AppTable.vue'
-import TurmasCursos from './../components/AppTurmasCursos.vue'
+import AppTable from './../components/AppTable.vue'
+import AppTurmasCursos from './../components/AppTurmasCursos.vue'
 
 Vue.use(VueRouter)
 
 export default new VueRouter ({
     linkExactActiveClass: 'link-active',
-    routes: [{
+    routes: [
+      {
         path: '/disciplinas',
-        component: Disciplinas        
-    }, {
+        component: AppTable
+      },
+      {
         path: '/tumascursos',
-        component: TurmasCursos
-    }]
+        component: AppTurmasCursos
+      }
+    ]
 })
