763ba5eaee342c791b92dc8e26886e08a9432feb
Taíssa <taissa.leal@estudante.ufjf.br>
2024-05-22 16:10:45 -0300

    Adiciona página Turmas-Cursos

--- src/components/AppTurmasCursos.vue
+++ src/components/AppTurmasCursos.vue
@@ -1,5 +1,266 @@
 <template>
-  <div></div>
+  <div>
+    <div class="mt-3" style="width: 841px; height: 871px;">
+      <h2 style="border-bottom: 1px solid #21252954;">Turmas-Cursos</h2>
+      <table class="table table-bordered table-striped">
+        <thead>
+          <tr style="background-color: #ec6b6f; color: white; font-weight: bold;">
+            <th>Código</th>
+            <th>Curso</th>
+            <th>Período</th>
+            <th>Cod. Disciplina</th>
+            <th>Turma</th>
+            <th>Horário</th>
+            <th>Vagas</th>
+            </tr>
+        </thead>
+
+        <tbody>
+          <tr>
+            <td>01B</td>
+            <td>Ciências Biológicas</td>
+            <td> </td>
+            <td> </td>
+            <td> </td>
+            <td> </td>
+            <td> </td>
+          </tr>
+
+          <tr>
+            <td> </td>
+            <td> </td>
+            <td>1</td>
+            <td>DC5199</td>
+            <td>BB</td>
+            <td>3a 10-12</td>
+            <td>1</td>
+          </tr>
+
+          <tr>
+            <td> </td>
+            <td> </td>
+            <td>1</td>
+            <td>DC5199</td>
+            <td>A</td>
+            <td>2a 08-10 / 5a 08-10</td>
+            <td>1</td>
+          </tr>
+
+          <tr>
+            <td>24A</td>
+            <td>Engenharia Civil</td>
+            <td> </td>
+            <td> </td>
+            <td> </td>
+            <td> </td>
+            <td> </td>
+          </tr>
+
+          <tr>
+            <td> </td>
+            <td> </td>
+            <td>1</td>
+            <td>DC5199</td>
+            <td>BB</td>
+            <td>3a 10-12</td>
+            <td>50</td>
+          </tr>
+
+          <tr>
+            <td> </td>
+            <td> </td>
+            <td>1</td>
+            <td>DC5199</td>
+            <td>E</td>
+            <td>3a 19-21</td>
+            <td>2</td>
+          </tr>
+
+          <tr>
+            <td> </td>
+            <td> </td>
+            <td>1</td>
+            <td>DC5199</td>
+            <td>G</td>
+            <td>4a 08-10</td>
+            <td>2</td>
+          </tr>
+
+          <tr>
+            <td> </td>
+            <td> </td>
+            <td>1</td>
+            <td>DC5199</td>
+            <td>GG</td>
+            <td>4a 08-10</td>
+            <td>2</td>
+          </tr>
+
+          <tr>
+            <td> </td>
+            <td> </td>
+            <td>1</td>
+            <td>DCC008</td>
+            <td>A</td>
+            <td>2a 10-12 / 5a 10-12</td>
+            <td>60</td>
+          </tr>
+
+          <tr>
+            <td> </td>
+            <td> </td>
+            <td>1</td>
+            <td>DCC199</td>
+            <td>C</td>
+            <td>2a 08-10 / 5a 08-10</td>
+            <td>50</td>
+          </tr>
+
+          <tr>
+            <td> </td>
+            <td> </td>
+            <td>1</td>
+            <td>DCC199</td>
+            <td>E</td>
+            <td>2a 14-16 / 5a 14-16</td>
+            <td>3</td>
+          </tr>
+
+          <tr>
+            <td> </td>
+            <td> </td>
+            <td>1</td>
+            <td>DCC199</td>
+            <td>H</td>
+            <td>2a 19-21 / 5a 19-21</td>
+            <td>3</td>
+          </tr>
+
+          <tr>
+            <td>26A</td>
+            <td>Administração Diurno</td>
+            <td> </td>
+            <td> </td>
+            <td> </td>
+            <td> </td>
+            <td> </td>
+          </tr>
+      
+          <tr>
+            <td> </td>
+            <td> </td>
+            <td>1</td>
+            <td>DCC133</td>
+            <td>A</td>
+            <td>2a 14-16 / EAD</td>
+            <td>46</td>
+          </tr>
+
+          <tr>
+            <td> </td>
+            <td> </td>
+            <td>1</td>
+            <td>DCC166</td>
+            <td>A</td>
+            <td>2a 21-23 / 4a 19-21</td>
+            <td>3</td>
+          </tr>
+
+          <tr>
+            <td>35A</td>
+            <td>Ciência da Computação Noturno</td>
+            <td> </td>
+            <td> </td>
+            <td> </td>
+            <td> </td>
+            <td> </td>
+          </tr>
+
+          <tr>
+            <td> </td>
+            <td> </td>
+            <td>1</td>
+            <td>DC5199</td>
+            <td>D</td>
+            <td>3a 16-18</td>
+            <td>2</td>
+          </tr>
+
+          <tr>
+            <td> </td>
+            <td> </td>
+            <td>1</td>
+            <td>DC5199</td>
+            <td>J</td>
+            <td>4a 17-19</td>
+            <td>2</td>
+          </tr>
+
+          <tr>
+            <td> </td>
+            <td> </td>
+            <td>1</td>
+            <td>DC5200</td>
+            <td>B</td>
+            <td>2a 19-21</td>
+            <td>45</td>
+          </tr>
+
+          <tr>
+            <td> </td>
+            <td> </td>
+            <td>1</td>
+            <td>DCC001</td>
+            <td>A</td>
+            <td>2a 16-18 / 6a 16-18</td>
+            <td>15</td>
+          </tr>
+
+          <tr>
+            <td> </td>
+            <td> </td>
+            <td>1</td>
+            <td>DCC008</td>
+            <td>E</td>
+            <td>2a 21-23 / 5a 21-23</td>
+            <td>40</td>
+          </tr>
+          
+          <tr>
+            <td> </td>
+            <td> </td>
+            <td>1</td>
+            <td>DCC012</td>
+            <td>A</td>
+            <td>2a 21-23 / 4a 19-21</td>
+            <td>26</td>
+          </tr>
+
+          <tr>
+            <td> </td>
+            <td> </td>
+            <td>1</td>
+            <td>DCC014</td>
+            <td>A</td>
+            <td>2a 14-16 / 4a 16-18</td>
+            <td>8</td>
+          </tr>
+
+          <tr>
+            <td> </td>
+            <td> </td>
+            <td>1</td>
+            <td>DCC019</td>
+            <td>A</td>
+            <td>3a 21-23 / 5a 21-23</td>
+            <td>25</td>
+          </tr>
+
+        </tbody>
+
+      </table>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -19,8 +280,8 @@ export default {
 }
 
 .table th, .table td {
-  padding: 8px;
-  text-align: left;
+  padding: 1px;
+  text-align: center;
   border-bottom: 1px solid #ddd;
 }
 
@@ -53,4 +314,9 @@ export default {
 .card, option, select {
   font-size: 12px;
 }
+
+.table tbody tr:hover {
+  background-color: #d5cfcf;
+}
+
 </style>
